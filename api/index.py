5f22803890ea6843632988a8d601baf81a01b5b3
yasir <yasiramunandar@gmail.com>
2022-07-12 22:38:12 +0700

    up

--- api/index.py
+++ api/index.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('swaggerui.html')
+    # return render_template('index.html')
 
 
 @app.route('/test')
